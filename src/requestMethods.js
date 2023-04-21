9dfbeb98da3201e989d42cc985a011973923d40b
Devlope Code <hitesh.gurjar786@gmail.com>
2023-04-21 12:57:54 +0530

    Bug Fixed

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const BASE_URL = "http://localhost:3001/api/";
-const BASE_URL = 'https://redzari.onrender.com/api/'
+const BASE_URL = 'https://redzari-backend.onrender.com/api/'
 const Token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjIwZDEzNTFiZDI2NWEyNGRjOWU0MDRlIn0sImlhdCI6MTY0NTUwNzI2NH0.u7jsguj9vhiZ7V6qL9DXVBsJm0XkhUaWkLEf74wHXFk";
 
